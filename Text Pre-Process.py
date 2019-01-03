8a99d8b6d7595a11686b092887404ece78d11f4f
KwstasChasapas <36344040+kwstas94@users.noreply.github.com>
2019-01-03 20:07:30 +0200

    Update and rename NLP.py to Text Pre-Process.py

--- Text Pre-Process.py
+++ Text Pre-Process.py
@@ -22,7 +22,7 @@ def remove_emoji(string):
                        u"\U0001F600-\U0001F64F"  # emoticons
                        u"\U0001F300-\U0001F5FF"  # symbols & pictographs
                        u"\U0001F680-\U0001F6FF"  # transport & map symbols
-                       u"\U0001F1E0-\U0001F1FF"  # flags (iOS)
+                       u"\U0001F1E0-\U0001F1FF"  # flags
                        u"\U00002702-\U000027B0"
                        u"\U000024C2-\U0001F251"
                        u"\U0001f926-\U0001f937"
